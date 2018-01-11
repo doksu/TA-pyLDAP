7707d60607ef2ec910cfcac9c29bc00c3d1f276e
Trusted Subject <trustedsubject@gmail.com>
2018-01-11 12:31:46 +1000

    v0.2.0

--- bin/ldapquery.py
+++ bin/ldapquery.py
@@ -8,25 +8,18 @@ import ldap
 @Configuration(type='reporting')
 class LDAPQueryCommand(GeneratingCommand):
 
-    limit = Option(require=False, validate=validators.Integer(minimum=1))
-    output = Option(require=False)
-    server = Option(require=True)
-    port = Option(require=True, validate=validators.Integer(minimum=1))
-    tls = Option(require=True, validate=validators.Boolean())
+    uri = Option(require=True)
     verifycert = Option(require=False, validate=validators.Boolean())
     scope = Option(require=False)
     basedn = Option(require=True)
     binddn = Option(require=False)
     bindpassword = Option(require=False)
-    filter = Option(require=True)
+    ldapfilter = Option(require=False)
     attributelist = Option(require=False)
 
     def generate(self):
 
         # Phase 1: Preparation
-
-        if not self.verifycert:
-            self.verifycert = True
 
         if self.scope:
             if self.scope == "base":
@@ -40,16 +33,47 @@ class LDAPQueryCommand(GeneratingCommand):
         else:
             self.scope = ldap.SCOPE_SUBTREE
 
+        if not self.ldapfilter:
+            self.ldapfilter = "(objectClass=*)"
+
         if self.attributelist:
             self.attributelist = str(self.attributelist).split()
         else:
             self.attributelist = None
 
-        # Phase 2: Running the search
+        # Phase 2: Connect to LDAP server and run search
 
-        l = ldap.initialize("ldaps://" + str(self.server) + ":" + str(self.port))
-        l.set_option(ldap.OPT_X_TLS_DEMAND, self.verifycert)
-        result_id = l.search(self.basedn, self.scope, self.filter, self.attributelist)
+        l = ldap.initialize(str(self.uri))
+
+        try:
+            if self.verifycert:
+                l.set_option(ldap.OPT_X_TLS_DEMAND, self.verifycert)
+        except:
+            pass
+
+        if self.binddn:
+            if self.bindpassword:
+                try:
+                    l.simple_bind_s(self.binddn, self.bindpassword)
+                except ldap.INVALID_CREDENTIALS:
+                    raise Exception("Credentials provided failed to bind to directory.")
+                except:
+                    raise Exception("Failed to bind to directory. Please check arguments provided and network connectivity.")
+            else:
+                raise Exception("Please provide bindpassword argument for authenticated binds.")
+
+        try:
+            # run the search
+            result_id = l.search(self.basedn, self.scope, self.ldapfilter, self.attributelist)
+
+        except ldap.LDAPError, error:
+            if type(error.message) == dict and error.message.has_key('desc'):
+                raise Exception(str(error.message['desc']))
+            else:
+                raise Exception(str(error))
+
+        except:
+            raise Exception("LDAP query failed. Please check arguments provided and network connectivity.")
 
         # Phase 3: Send results to Splunk
 
@@ -63,12 +87,10 @@ class LDAPQueryCommand(GeneratingCommand):
             result_type, result_data = l.result(result_id, 0)
             if result_type == ldap.RES_SEARCH_ENTRY:
                     # for future reference the entries used below looks like this: [('uid=username,ou=users,dc=example,dc=com', {'cn': ['Example'], 'mail': ['username@example.com']})]
-                    l.set_option(ldap.OPT_X_TLS_DEMAND, self.verifycert)
                     entry = result_data[0][1]
                     entry["dn"] = result_data[0][0]
                     attributes.update(entry.keys())
                     cache += [entry]
-                    del entry
             else:
                     break
 
@@ -79,5 +101,8 @@ class LDAPQueryCommand(GeneratingCommand):
                     entry[attribute] = []
 
             yield entry
+
+        if self.binddn:
+            l.unbind_s()
 
 dispatch(LDAPQueryCommand, sys.argv, sys.stdin, sys.stdout, __name__)
